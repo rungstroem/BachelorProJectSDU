afa60e6a7679034db3960686220f1ba31d00bac0
rungstroem <32567785+rungstroem@users.noreply.github.com>
2020-02-19 08:22:02 +0100

    Add files via upload

---
+++ piShutdown.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python 
+import RPi.GPIO as GPIO 
+import subprocess
+# Starting up 
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(3, GPIO.IN)
+# Wait until power button is off 
+# Recommended to use GPIO.BOTH for cases with switch 
+GPIO.wait_for_edge(3, GPIO.BOTH)
+# Shutting down 
+subprocess.call(['shutdown', '-h', 'now'], shell=False)
+
